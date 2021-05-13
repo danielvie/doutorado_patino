e07fb194783fb2cdecc52536533a4481f94891dc
daniel <danielvie@gmail.com>
2021-05-12 22:38:34 -0300

    correcao plot funcao custo

--- +doc/fun_custo_patino.m
+++ +doc/fun_custo_patino.m
@@ -25,35 +25,37 @@ function J = fun_custo_patino(config, X)
     
     y_ = y - y(1,:);
     
-    figure(4);
-    plot(y_(:,1), y_(:,2)); hold on;
-    plot(y_(1,1), y_(1,2), 's');
-    plot(y_(end,1), y_(end,2), 'o');
-    
-    axis([-0.5, 1.5, -0.5, 1]);
-    title('Tensao C1 vs Tensao C2')
-    grid on;
-    hold off;
-    
-    figure(5);
-    plot(y_(:,1), y_(:,3)); hold on;
-    plot(y_(1,1), y_(1,3), 's');
-    plot(y_(end,1), y_(end,3), 'o');
-    
-    axis([-0.5, 1.5, -0.5, 0.5]);
-    title('Tensao C1 vs Corrente Il')
-    grid on;
-    hold off;
-    
-    figure(6);
-    plot(y_(:,2), y_(:,3)); hold on;
-    plot(y_(1,2), y_(1,3), 's');
-    plot(y_(end,2), y_(end,3), 'o');
-    
-    axis([-0.5, 1.0, -0.3, 0.3]);
-    title('Tensao C2 vs Corrente Il')
-    grid on;
-    hold off;
+    if size(y_, 2) > 2
+        figure(4);
+        plot(y_(:,1), y_(:,2)); hold on;
+        plot(y_(1,1), y_(1,2), 's');
+        plot(y_(end,1), y_(end,2), 'o');
+
+        axis([-0.5, 1.5, -0.5, 1]);
+        title('Tensao C1 vs Tensao C2')
+        grid on;
+        hold off;
+
+        figure(5);
+        plot(y_(:,1), y_(:,3)); hold on;
+        plot(y_(1,1), y_(1,3), 's');
+        plot(y_(end,1), y_(end,3), 'o');
+
+        axis([-0.5, 1.5, -0.5, 0.5]);
+        title('Tensao C1 vs Corrente Il')
+        grid on;
+        hold off;
+
+        figure(6);
+        plot(y_(:,2), y_(:,3)); hold on;
+        plot(y_(1,2), y_(1,3), 's');
+        plot(y_(end,2), y_(end,3), 'o');
+
+        axis([-0.5, 1.0, -0.3, 0.3]);
+        title('Tensao C2 vs Corrente Il')
+        grid on;
+        hold off;
+    end
     
     % axis([-0.1, 2, -0.1, 2, -0.2, 0.2]);
     
